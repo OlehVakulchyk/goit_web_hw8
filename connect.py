bce8d1deea5815e9153bb0a8352b81f0e4d8173b
OlehVakulchyk <4020405@gmail.com>
2023-04-17 14:31:21 +0300

    Finish mongoengine part
    without Redis

--- connect.py
+++ connect.py
@@ -1,18 +1,3 @@
-# from mongoengine import *
-# from pymongo.mongo_client import MongoClient
-
-# uri = "mongodb+srv://goit-web10:131065@goitlearn-10.m8bniak.mongodb.net/test?retryWrites=true&w=majority"
-
-# # Create a new client and connect to the server
-# client = MongoClient(uri)
-
-# # Send a ping to confirm a successful connection
-# try:
-#     client.admin.command('ping')
-#     print("Pinged your deployment. You successfully connected to MongoDB!")
-# except Exception as e:
-#     print(e)
-
 from mongoengine import connect
 import configparser
 
--- models.py
+++ models.py
@@ -2,8 +2,6 @@ from mongoengine import *
 
 import connect
 
-# connect(host="mongodb+srv://goit-web10:131065@goitlearn-10.m8bniak.mongodb.net/test?retryWrites=true&w=majority")
-
 
 class Author(Document):
     fullname = StringField(required=True)
@@ -15,30 +13,3 @@ class Quote(Document):
     author = ReferenceField(Author, reverse_delete_rule=CASCADE)
     tags = ListField(StringField(max_length=30))
     content = StringField()
-    # meta = {'allow_inheritance': True}
-
-
-
-# class User(Document):
-#     email = StringField(required=True)
-#     first_name = StringField(max_length=50)
-#     last_name = StringField(max_length=50)
-
-
-# class Post(Document):
-#     title = StringField(max_length=120, required=True)
-#     author = ReferenceField(User, reverse_delete_rule=CASCADE)
-#     tags = ListField(StringField(max_length=30))
-#     meta = {'allow_inheritance': True}
-
-
-# class TextPost(Post):
-#     content = StringField()
-
-
-# class ImagePost(Post):
-#     image_path = StringField()
-
-
-# class LinkPost(Post):
-#     link_url = StringField()
